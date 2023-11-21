d50c83a1f12e9ed3a89c0c5a425b98caf1995fe0
byron korir <byron.korir@student.moringaschool.com>
2023-11-21 14:29:16 +0300

    complete assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,32 @@
 
 def happy_new_year():
     # code goes here!
+    countdown = 10
+    while countdown >=1:
+        print(countdown)
+        countdown -=1
+    print('Happy New Year!')    
+   
     pass
 
 def square_integers(int_list):
     # code goes here!
+    squared_int = [n**2 for n in int_list]
+    return squared_int
+
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print( 'FizzBuzz')
+        elif i % 3 == 0:
+            print( 'Fizz')
+        elif i % 5 == 0:
+            print( 'Buzz')
+        else:
+            print(i)
+        
+       
     pass
